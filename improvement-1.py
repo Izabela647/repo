980cf2687b8e34c2b69067a28eb2525baf1bae5d
Izabela <ijanuszkiewicz611@gmail.com>
2024-03-08 21:10:44 +0000

    Commit message for submodule changes

---
+++ improvement-1.py
@@ -0,0 +1,7 @@
+data = input("Please enter a number between 1 and 10:  ")   # Prompting user for input
+if data >= 10:      # Creating a condition using an if-statement for the user's number to be up to or equal to 10.
+    print("Yes you have successfully entered a number!")  # Printing a relevant statement to user.
+else:
+    print("You're number is out of range")   # If user does not enter a number up to or equal to 10, they will be
+                                             # provided with a relevant print statement.
+
